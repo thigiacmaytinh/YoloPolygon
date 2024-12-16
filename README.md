764606c34e1be663e2ce70ddfe19623485979a54
vohungvi <vohungvi@vohungvi.com>
2024-12-16 15:35:40 +0700

    !!! release 0.0.2

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # YoloPolygon
+
+<strong>YOLO Polygon</strong> là phần mềm do chúng tôi phát triển để đánh nhãn vật thể training segmentation. 
+
+![](images/yolo-polygon.jpg)
+
+Với bài toán phát hiện đối tượng nằm cạnh nhau, có hình dạng không phải đa giác lồi có thể chồng chập lên nhau thì phải vẽ đường gấp khúc bao lấy đối tượng. Công cụ YOLO Polygon giúp các bạn đánh nhãn dễ dàng trên Windows. 
+
+Đây là chương trình viết bằng ngôn ngữ C#, sử dụng .NET framework nên dễ dàng build và chỉnh sửa theo ý các bạn.
+
+<strong>Đặc biệt: </strong>chương trình có full source code
+
+Link: https://thigiacmaytinh.com/yolo-polygon-phan-mem-danh-nhan-yolo-segmentation-tren-windows
--- YoloPolygon/Properties/AssemblyInfo.cs
+++ YoloPolygon/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Yolo Polyline")]
-[assembly: AssemblyDescription("Yolo Polyline")]
+[assembly: AssemblyTitle("Yolo Polygon")]
+[assembly: AssemblyDescription("Yolo Polygon")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("VisCom Solution")]
-[assembly: AssemblyProduct("YOLO label")]
-[assembly: AssemblyCopyright("Copyright © VisCom Solution 2022")]
+[assembly: AssemblyProduct("YOLO Polygon")]
+[assembly: AssemblyCopyright("Copyright © VisCom Solution 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
